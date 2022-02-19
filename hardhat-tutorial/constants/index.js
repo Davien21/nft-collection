73e825c22ab471a30bad8a5df53983d63cade95b
davien21 <chidiebereekennia@gmail.com>
2022-02-19 13:27:00 +0100

    --feat: update base url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x0e1244b67898CBF1b03213E243b91121BDc2a4D2";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://chidi-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
